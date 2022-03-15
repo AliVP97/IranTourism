1fc41faeb9ffeb336bef29b594fcb8b5a783a383
alivp97 <ali.valipour@kntu.ac.ir>
2022-03-15 13:02:10 +0330

    geocoding util

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,16 +1,11 @@
 import axios from 'axios';
 
-export default function geocode(latitude, logitude) {
+export default function geocode(latitude, longitude, language = 'fa') {
   const access_token =
     'pk.eyJ1IjoiYWxpdnAiLCJhIjoiY2wwamhhMGR2MDBsMjNjbDFwZXR1cDZ2NCJ9.KpC8xYmNBj0KgJ7lZ-zTVw';
-  var config = {
-    method: 'get',
-    url: `https://api.mapbox.com/geocoding/v5/mapbox.places/${logitude},${latitude}.json?access_token=${access_token}`,
-  };
 
-  axios(config)
-    .then(res => {
-      console.log(res);
-    })
-    .catch(error => console.log(error));
+  return axios.get(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json`,
+    {params: {access_token: access_token, language: language}},
+  );
 }
